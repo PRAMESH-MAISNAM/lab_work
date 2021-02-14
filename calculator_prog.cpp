f9f346d1dd0bcf68bf906fbfd1f609d500ebb508
Pramesh Maisnam <prameshmaisnam@gmail.com>
2021-02-14 21:42:51 +0530

    First commit

---
+++ calculator_prog.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char operat;
+    double first, second;
+    printf("Enter an operator (+, -, *,/): ");
+    scanf("%c", &operat);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &first, &second);
+
+    switch (operat)
+	{
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+        break;
+    case '-':
+        printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+        break;
+    case '*':
+        printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+        break;
+    case '/':
+        printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+        break;
+        // operator doesn't match any case constant
+    default:
+        printf("Error! operator is not correct");
+    }
+
+    return 0;
+    getch();
+}
